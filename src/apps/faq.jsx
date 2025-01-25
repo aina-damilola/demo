4c5b9468c9837ddefb9e12305b7ce12b20e6d184
Dami <damilola.aina18@gmail.com>
2025-01-25 10:54:58 -0500

    added mobile compatibility for the headers of the othjer sections and faq section

--- src/apps/faq.jsx
+++ src/apps/faq.jsx
@@ -27,12 +27,40 @@ function Question(props){
     )
 }
 
+/*
+AI is  a broad term that we define within the scope of NSBEHacks as a part of one of the following classes:
+                <br/>
+                <ul>
+                    <li>
+                        Generative AI: Leveraging AI to create media such as audio, text, images, and video.
+                    </li>
+                    <li>
+                        Predictive AI: Using AI to forecast events or outcomes for preventative or proactive actions, such as climate risk analysis or personalized medicine.
+                    </li>
+                    <li>
+                        Security AI: Implementing AI for enhanced protection, including facial recognition, breach detection, and anomaly identification.
+                    </li>
+                    <li>
+                        Assistive AI: Tools that augment human capabilities, such as accessibility devices or cognitive aids for education and work.
+                    </li>
+                    <li>
+                        Optimizing AI: Systems designed to improve efficiencies in processes like supply chains, energy consumption, or urban planning.
+                    </li>
+                </ul>
+*/
+
 function FAQ({section5Ref}){
     return(
         <div id="main-container-faq" ref={section5Ref}>
             <h1>FAQs</h1>
             <div id="faqs-container">
-                <Question question="" answer=""/>
+                <Question question="How many people per team?" answer="Each team should have within X-Y people in the team but a concrete list of persons are not needed upon registration."/>
+                
+                <Question question="Is NSBEHacks only for black identifying people?" answer="No, NSBEHacks welcomes everyone from all demographics. We strive for an inclusive environment where inclusion and learning is at the forefront."/>
+
+                <Question question="What is the theme for NSBEHacks 2025?" answer={<>NSBEHacks invites participants to explore the mindful integration, use, and creation of artificial intelligence (AI) to solve pressing challenges. With the theme “AI with Purpose,” we challenge participants to design projects where AI is not just a tool but a transformative force, solving problems that would be otherwise impossible without it. Participants are encouraged to focus on intentional and ethical uses of AI, creating solutions that demonstrate unique value derived from AI technologies.</>}/>
+
+                <Question question="Can I register for NSBEHacks if I'm not in University of Toronto?" answer="Yes! NSBEHacks welcomes everyone from all institutions and skill levels to join in the hackathon."/>
             </div>
         </div>
     )
